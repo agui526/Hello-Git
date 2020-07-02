2cfe1339e656a0b3970ee66de88415628854e409
agui526 <bishuwu@foxmail.com>
2020-07-02 21:09:50 +0800

    first commot

---
+++ git_demo/src/com/test/HelloGit.java
@@ -0,0 +1,13 @@
+package com.test;
+/**
+ *    @author    作者
+ *    @Date 2020-07-02 21:07:00    
+ *
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello git...");
+	}
+	
+}
